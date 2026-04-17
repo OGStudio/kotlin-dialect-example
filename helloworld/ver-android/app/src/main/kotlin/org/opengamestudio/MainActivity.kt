5a4617fdc44f4a625898e7eb41ec2998563cf4e3
Михаил Капелько <kornerr@gmail.com>
2026-04-17 22:20:25 +0300

    some

--- helloworld/ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
+++ helloworld/ver-android/app/src/main/kotlin/org/opengamestudio/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity: ComponentActivity() {
 
         setContent {
             MyApplicationTheme {
-                MainView(vm = vm)
+                MainView(VM)
             }
         }
     }
