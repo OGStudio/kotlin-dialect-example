c383daba31fab9818b0e56a23b9d3d2b022e458e
Михаил Капелько <kornerr@gmail.com>
2026-06-24 22:10:48 +0300

    print items

--- hellolist/components/root/desktop/rootEffect.cpp
+++ hellolist/components/root/desktop/rootEffect.cpp
@@ -8,7 +8,8 @@ void rootResetItems(const VM &vm) {
     auto rootCtx = KT.rootCtrlCtx();
     auto items = RootContext_items(rootCtx);
     for (int i = 0; i < items.size(); ++i) {
-        printf("ИГР rootE.rootRE item id: '%d'\n", i);
+        auto item = items[i];
+        printf("ИГР rootE.rootRE item id/title: '%d'/'%s'\n", item.id(), item.title().toStdString().c_str());
     }
 }
 
